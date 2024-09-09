4169a84f64fa17b0e60f86fff875261ec685b943
Tade03 <tade.castillo.03@gmail.com>
2024-09-09 15:51:44 -0600

    Bootstrap, Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './Pagina/**/*.{html,js}',  // Busca en los archivos HTML y JS dentro de Pagina
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  
+  ],
+}
